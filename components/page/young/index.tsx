1b8462e2781529b814e725892f9b9131fdb55255
Young <young20050727@gmail.com>
2021-12-17 11:25:31 +0800

    init

---
+++ components/page/young/index.tsx
@@ -0,0 +1,12 @@
+import './young.sass';
+import { Text,View } from '../../Themed';
+
+function Young(){
+    return(
+        <View>
+            <Text>Young好帥</Text>
+        </View>
+    )
+}
+
+export default Young;
